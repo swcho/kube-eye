e6cddb9319bec09e8c3e2fadb61c7b3cbb4aa956
Sungwoo <sungwoo.cho.dev@gmail.com>
2019-02-13 21:39:05 +0900

    ExecView Clean Up

--- src/components/ExecView.tsx
+++ src/components/ExecView.tsx
@@ -16,46 +16,55 @@ function b64_to_utf8(str: string) {
   return decodeURIComponent(window['escape'](window.atob(str)));
 }
 
+const DEFAULT_COLS = 120;
+const DEFAULT_ROWS = 25;
+
 class ExecView extends React.Component<ExecView.Props> {
   private elXterm: HTMLDivElement | undefined | null;
+
+  // https://github.com/kubernetes-ui/container-terminal/blob/master/container-terminal.js
+  private ws: WebSocket | undefined;
+  private xterm: Terminal;
+  private cols: number;
+  private rows: number;
+
+  constructor(props: ExecView.Props) {
+    super(props);
+    this.cols = DEFAULT_COLS;
+    this.rows = DEFAULT_ROWS;
+    this.xterm = new Terminal({
+      cols: this.cols,
+      rows: this.rows,
+    });
+  }
+
   render() {
     const {
-      pod,
-      containerName: cotainerName,
-      onClose
+      containerName,
     } = this.props;
     return (
       <div>
         <Overlay isOpen={true} onOpened={this.handleOpened}>
           <div className={S.ExecView}>
             <div className={S.inner}>
-              <div className={S.name}>{cotainerName}</div>
+              <h1 className={S.name}>{containerName}</h1>
               <div className={S.terminal} ref={(el) => (this.elXterm = el)} />
-              <Button onClick={onClose}>Close</Button>
+              <Button onClick={() => this.handleClose()}>Close</Button>
             </div>
           </div>
         </Overlay>
       </div>
     );
   }
-
   handleOpened = async () => {
     const { api, pod, containerName } = this.props;
     if (!api || !this.elXterm) {
       return;
     }
     try {
-      const defaultCols = 120;
-      const defaultRows = 25;
-      const cols = defaultCols;
-      const rows = defaultRows;
-      const xterm = new Terminal({
-        cols,
-        rows,
-      });
-      xterm.open(this.elXterm);
+      this.xterm.open(this.elXterm);
 
-      const ws = api.pods().exec(
+      this.ws = api.pods().exec(
         pod,
         {
           container: containerName,
@@ -69,36 +78,57 @@ class ExecView extends React.Component<ExecView.Props> {
 
       let alive;
       let first = true;
-      ws.onopen = (ev) => {
-        alive = setInterval(() => ws.send('0'), 30 * 1000);
+      this.ws.onopen = (ev) => {
+        alive = setInterval(() => this.ws && this.ws.send('0'), 30 * 1000);
       };
-      ws.onmessage = (ev) => {
+      this.ws.onmessage = (ev) => {
         const data = ev.data.slice(1);
         console.log('onmessage', data);
         switch (ev.data[0]) {
           case '1':
           case '2':
           case '3':
-            xterm.write(b64_to_utf8(data));
+            this.xterm.write(b64_to_utf8(data));
             break;
           default:
         }
         if (first) {
           first = false;
-          xterm.focus();
+          this.xterm.focus();
         }
       };
+      this.ws.onclose = (ev) => {
+        this.handleClose(ev.reason, first);
+      };
 
-      xterm.on('data', (data) => {
+      this.xterm.on('data', (data) => {
         // ws.emit('data', data);
-        if (ws.readyState === 1) {
+        if (this.ws && this.ws.readyState === 1) {
           console.log('xterm.data', data);
-          ws.send('0' + utf8_to_b64(data));
+          this.ws.send('0' + utf8_to_b64(data));
         }
       });
     } catch (e) {
       console.error(e);
     }
+  }
+
+  handleClose(message?: string, first?: boolean) {
+    if (!message && first) {
+        message = 'Could not connect to the container. Do you have sufficient privileges?';
+    }
+    if (!message) {
+        message = 'disconnected';
+    }
+    if (!first) {
+        message = '\r\n' + message;
+    }
+    if (this.ws) {
+      this.ws.close();
+    }
+    this.xterm.write('\x1b[31m' + message + '\x1b[m\r\n');
+    this.xterm.dispose();
+    this.props.onClose();
   }
 }
 
